728f2ebd4569039883bca9cb304b8238a82886b2
pawan1211 <63227421+pawan1211@users.noreply.github.com>
2020-04-11 00:40:23 +0530

    Update assignment_1_second_part.py

--- python_L2_assignment/assignment_1_second_part.py
+++ python_L2_assignment/assignment_1_second_part.py
@@ -1,3 +1,14 @@
+"""
+1.	Write a code to implement the package  below and distribute it.
+Mathnew package with following modules
+1. sqroot
+2. addition
+3. subtraction
+4. multiplication
+5. division
+"""
+
+
 import Mathnew 
 Mathnew.division(1,4)
 Mathnew.multiplication(1,6)
